47b9bc87241c50f68d19dc1a1119ad596ec3c483
Joseph Edwards Rean <joseph.edwardsrean@outlook.com>
2022-06-07 15:00:42 +0000

    Altered readme and completed step 9

--- directory.rb
+++ directory.rb
@@ -3,10 +3,9 @@ def input_students
   puts "To finish, just hit return twice"
   
   students = []
-  
   name = gets.chomp
   
-  while !name.empty? do
+  while name.empty? == false do
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     name = gets.chomp
@@ -20,16 +19,41 @@ def print_header
 end 
 
 def print(students)
-  students.each do |student|
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  if students.count >0
+    students.each do |student|
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  if students.count == 1
+    puts "Overall, we have 1 amazing student"
+  else
+    puts "Overall, we have #{students.count} great students"
+  end
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+    end
+  end
+end
+
+interactive_menu
